8440e854d4952c09c32fbeb66eb4986202ab68a0
zhangchaochao <zhangchaochao@iresearch.com.cn>
2021-07-20 10:57:44 +0800

    redis lock

--- one-boot-core/src/main/java/com/one/boot/core/cache/RedisService.java
+++ one-boot-core/src/main/java/com/one/boot/core/cache/RedisService.java
@@ -4,7 +4,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.util.CollectionUtils;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.params.SetParams;
 
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -16,10 +20,25 @@ import java.util.concurrent.TimeUnit;
 @Slf4j
 public class RedisService {
 
+    private final String SCRIPT_LOCK_LUA = "if redis.call('get',KEYS[1]) == ARGV[1] then return redis.call('del',KEYS[1]) else return 0  end";
+
     @Autowired(required = false)
     private RedisTemplate redisTemplate;
 
+    @Autowired(required = false)
+    private JedisPool jedisPool;
+
+    private String LOCK_KEY = "redis_lock"; //锁键
+
+    private long internalLockLeaseTime = 30000;//锁过期时间
+
+    private long timeout = 100000; //获取锁的超时时间
+
+    //SET命令的参数
+    private SetParams params = SetParams.setParams().nx().px(internalLockLeaseTime);
+
     public boolean expire(String key, long time) {
+
         try {
             if (time > 0) {
                 this.redisTemplate.expire(key, time, TimeUnit.SECONDS);
@@ -82,6 +101,52 @@ public class RedisService {
         } catch (Exception e) {
             log.error(e.getMessage(), e);
             return false;
+        }
+    }
+
+    /**
+     * 加锁
+     *
+     * @param id
+     * @return
+     */
+    public boolean lock(String id) {
+        Jedis jedis = jedisPool.getResource();
+        Long start = System.currentTimeMillis();
+        try {
+            for (; ; ) {
+                //SET命令返回OK ，则证明获取锁成功
+                String lock = jedis.set(LOCK_KEY, id, params);
+                if ("OK".equals(lock)) {
+                    return true;
+                }
+                //否则循环等待，在timeout时间内仍未获取到锁，则获取失败
+                long l = System.currentTimeMillis() - start;
+                if (l >= timeout) {
+                    return false;
+                }
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    log.error(e.getMessage(), e);
+                }
+            }
+        } finally {
+            jedis.close();
+        }
+    }
+
+    public boolean unlock(String id) {
+        Jedis jedis = jedisPool.getResource();
+        try {
+            Object result = jedis.eval(SCRIPT_LOCK_LUA, Collections.singletonList(LOCK_KEY),
+                    Collections.singletonList(id));
+            if ("1".equals(result.toString())) {
+                return true;
+            }
+            return false;
+        } finally {
+            jedis.close();
         }
     }
 
